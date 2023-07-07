c8a5705ed98c76bcb2cec11ec1bb4ad267b22205
dav <73536483+Davizinhn@users.noreply.github.com>
2023-07-07 16:26:50 -0300

    coisos

--- Assets/Scripts/AIPessoa.cs
+++ Assets/Scripts/AIPessoa.cs
@@ -7,6 +7,11 @@ public class AIPessoa : MonoBehaviour
 {
     public Transform akinori;
     public NavMeshAgent agent;
+    public Animator anim;
+    public bool isGrounded = true;
+    public float distance;
+    public float normalSpeed;
+    public float jumpSpeed;
 
     void Start()
     {
@@ -15,6 +20,20 @@ public class AIPessoa : MonoBehaviour
 
     void Update()
     {
+        RaycastHit hit;
+        Ray ray = new Ray(this.gameObject.transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out hit, distance)) {
+            isGrounded = true;
+        }else{isGrounded=false;}
+        if(isGrounded)
+        {
+            agent.speed = normalSpeed;
+        }
+        else{
+            agent.speed = jumpSpeed;
+        }
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isRunning", agent.remainingDistance > 0.005f);
         agent.SetDestination(akinori.position);
     }
 }
