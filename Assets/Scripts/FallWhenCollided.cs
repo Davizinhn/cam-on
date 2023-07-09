c34bbb76bf016a06cec5ad0ae26c2ba0f2e5289d
dav <73536483+Davizinhn@users.noreply.github.com>
2023-07-08 21:00:40 -0300

    bugfixes

--- Assets/Scripts/FallWhenCollided.cs
+++ Assets/Scripts/FallWhenCollided.cs
@@ -7,21 +7,28 @@ public class FallWhenCollided : MonoBehaviour
 
     public bool ha = false;
     public bool hi = false;
+    bool didFellYet = false;
     
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag=="Player")
+        if(!didFellYet)
         {
-            col.gameObject.GetComponent<FirstPersonController>().enableJump = false;
-            gameObject.AddComponent<Rigidbody>();
-            this.gameObject.GetComponent<Collider>().isTrigger=ha;
-            this.gameObject.GetComponent<AudioSource>().Play();
-        }
-        if(hi)
-        {
+            if(col.gameObject.tag=="Player")
+            {
+                col.gameObject.GetComponent<FirstPersonController>().enableJump = !hi;
                 gameObject.AddComponent<Rigidbody>();
                 this.gameObject.GetComponent<Collider>().isTrigger=ha;
                 this.gameObject.GetComponent<AudioSource>().Play();
+                didFellYet = true;
+            }
+            if(hi)
+            {
+                    gameObject.AddComponent<Rigidbody>();
+                    this.gameObject.GetComponent<Collider>().isTrigger=ha;
+                    this.gameObject.GetComponent<AudioSource>().Play();
+                    didFellYet = true;
+            }
         }
+
     }
 }
